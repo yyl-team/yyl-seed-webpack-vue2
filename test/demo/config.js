9799ef98fe3422865b5f0280f871eae1c725e3a7
jackNEss <jackness1208@qq.com>
2018-09-16 08:43:00 +0800

    开发中

--- test/demo/config.js
+++ test/demo/config.js
@@ -1,199 +1,83 @@
+/* eslint indent: ["error", 2, { "VariableDeclarator": 1 }] */
 const path = require('path');
 
-// + yyl init 自动 匹配内容
-const COMMON_PATH = /* +commonPath */'../commons/mobile'/* -commonPath */;
-const PROJECT_NAME = /* +name */'workflow_demo'/* -name */;
-const VERSION = /* +version */'1.0.0'/* -version */;
-const PLATFORM = /* +platform */'mobile'/* -platform */;
-// - yyl init 自动 匹配内容
+const config = {};
 
+// + setting
 const setting = {
-  'localserver': { // 本地服务器配置
-    'root': path.join(__dirname, './dist'), // 服务器输出地址
-    'port': 5000 // 服务器 port
+  localserver: {
+    root: './dist'
   },
-  'dest': {
-    'basePath': '/website_static/mobileYY/test',
-    'jsPath': 'js',
-    'jslibPath': 'js/lib',
-    'cssPath': 'css',
-    'htmlPath': 'html',
-    'imagesPath': 'images',
-    'revPath': 'assets',
-    'tplPath': 'tpl'
-  },
-  // 代理服务器
-  'proxy': {
-    'port': 8887,
-    'localRemote': {
-      // 'http://www.yy.com/': './dist/',
-      'http://www.yy.com/': 'http://127.0.0.1:5000/',
-      'http://wap.yy.com/': 'http://127.0.0.1:5000/'
-    }
-  },
-  /**
-     * 触发提交 svn 前中间件函数
-     * @param {String}   sub    命令行 --sub 变量
-     * @param {Function} next() 下一步
-     */
-  onBeforeCommit(sub, next) {
-    next();
-  },
-
-  /**
-     * 初始化 config 时 对config的二次操作
-     * @param {object}   config          服务器初始化完成的 config 对象
-     * @param {object}   env             命令行接收到的 参数
-     * @param {function} next(newconfig) 返回给服务器继续处理用的 next 函数
-     * @param {object}   newconfig       处理后的 config
-     */
-  onInitConfig(config, env, next) {
-    next(config);
+  dest: {
+    basePath: '/pc',
+    jsPath: 'js',
+    jslibPath: 'js/lib',
+    cssPath: 'css',
+    htmlPath: 'html',
+    imagesPath: 'images',
+    tplPath: 'tpl',
+    revPath: 'assets'
   }
 };
+// - setting
 
-const config = {
-  'workflow': 'webpack-vue2',
-  'name': PROJECT_NAME,
-  'version': VERSION,
-  'dest': setting.dest,
-  'proxy': setting.proxy,
-  'platform': PLATFORM,
+const DEST_BASE_PATH = path.join(setting.localserver.root, setting.dest.basePath);
+const COMMON_PATH = '../commons';
 
-  'onInitConfig': setting.onInitConfig,
-  'onBeforeCommit': setting.onBeforeCommit,
+// + base
+Object.assign(config, {
+  localserver: setting.localserver,
+  dest: setting.dest
+});
+// - base
 
-  // 构建触发 eslint 检查
-  'eslint': true,
-
-  // 需要构建工具额外安装的 npm 组件放这里 如 axios
-  'plugins': ['axios'],
-  // +此部分 yyl server 端config 会进行替换
-  'localserver': setting.localserver,
-  'resource': { // 自定义项目中其他需打包的文件夹
-    /*
-    'src/swf': path.join(setting.localserver.root, setting.dest.basePath, 'swf'),
-    'src/font': path.join(setting.localserver.root, setting.dest.basePath, 'font')
-     */
-  },
-  // 对应 webpack.config 中 entry 字段
-  'entry': {
-    // 'vendors': ['flexlayout']
-  },
-  'alias': { // yyl server 路径替换地方
-
-    // svn dev 分支地址
-    'dev': path.join('../../../svn.yy.com/yy-music/web/publish/src/3g/mobile-website-static/trunk/mobileYY/test'),
-
-    // svn trunk 分支地址
-    'trunk': path.join('../../../svn.yy.com/yy-music/web/publish/src/3g/mobile-website-static/branches/release/mobileYY/test'),
-
-
-    // 公用组件地址
-    'commons': COMMON_PATH,
-
-    // 公用 components 目录
-    'globalcomponents': path.join(COMMON_PATH, 'components'),
-    'globallib': path.join(COMMON_PATH, 'lib'),
-
-
+// + alias
+Object.assign(config, {
+  alias: {
     // 输出目录中 到 html, js, css, image 层 的路径
-    'root': path.join(setting.localserver.root, setting.dest.basePath),
-
+    root: DEST_BASE_PATH,
     // rev 输出内容的相对地址
-    'revRoot': path.join(setting.localserver.root, setting.dest.basePath),
-
+    revRoot: DEST_BASE_PATH,
     // dest 地址
-    'destRoot': setting.localserver.root,
-
+    destRoot: setting.localserver.root,
     // src 地址
-    'srcRoot': path.join(__dirname, './src'),
-
+    srcRoot: './src',
     // 项目根目录
-    'dirname': __dirname,
-
+    dirname: './',
+    // 公用组件地址
+    commons: COMMON_PATH,
+    // 公用 components 目录
+    globalcomponents: path.join(COMMON_PATH, 'components'),
+    globallib: path.join(COMMON_PATH, 'lib'),
     // js 输出地址
-    'jsDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.jsPath),
+    jsDest: path.join(DEST_BASE_PATH, setting.dest.jsPath),
     // js lib 输出地址
-    'jslibDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.jslibPath),
+    jslibDest: path.join(DEST_BASE_PATH, setting.dest.jslibPath),
     // html 输出地址
-    'htmlDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.htmlPath),
+    htmlDest: path.join(DEST_BASE_PATH, setting.dest.htmlPath),
     // css 输出地址
-    'cssDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.cssPath),
+    cssDest: path.join(DEST_BASE_PATH, setting.dest.cssPath),
     // images 输出地址
-    'imagesDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.imagesPath),
+    imagesDest: path.join(DEST_BASE_PATH, setting.dest.imagesPath),
     // assets 输出地址
-    'revDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.revPath),
-
-    'tplDest': path.join(setting.localserver.root, setting.dest.basePath, setting.dest.tplPath),
-
+    revDest: path.join(DEST_BASE_PATH, setting.dest.revPath),
+    // tpl 输出地址
+    tplDest: path.join(DEST_BASE_PATH, setting.dest.tplPath),
     // webpackconfig 中的 alias
-    'flexlayout': path.join('./src/js/lib/flexLayout/flexLayout-1.4.2.js')
+    flexlayout: path.join('./src/js/lib/flexLayout/flexLayout-1.4.2.js')
     // + yyl make
     // - yyl make
-  },
-  // -此部分 yyl server 端config 会进行替换
-
-  // + 此部分 不要用相对路径
-  // = 用 {$变量名} 方式代替, 没有合适变量可以自行添加到 alias 上
-  'concat': {
-    '{$jsDest}/vendors.js': [
-      '{$flexlayout}'
-    ]
-  },
-
-  'commit': {
-    // 上线配置
-    'revAddr': 'http://s1.yy.com/website_static/mobileYY/test/assets/rev-manifest.json',
-    'hostname': 'http://s1.yy.com/',
-    'git': {
-      'update': []
-    },
-    'svn': {
-      'dev': {
-        'update': [
-          '{$dev}/assets',
-          '{$dev}/mobileYY/test'
-        ],
-        'copy': {
-          '{$root}/js': ['{$dev}/js'],
-          '{$root}/css': ['{$dev}/css'],
-          '{$root}/html': ['{$dev}/html'],
-          '{$root}/images': ['{$dev}/images'],
-          '{$root}/assets': ['{$dev}/assets']
-        },
-        'commit': [
-          '{$dev}/js',
-          '{$dev}/css',
-          '{$dev}/html',
-          '{$dev}/images',
-          '{$dev}/assets'
-        ]
-
-      },
-      'trunk': {
-        'update': [
-          '{$trunk}'
-        ],
-        'copy': {
-          '{$root}/js': ['{$trunk}/js'],
-          '{$root}/css': ['{$trunk}/css'],
-          '{$root}/html': ['{$trunk}/html'],
-          '{$root}/images': ['{$trunk}/images'],
-          '{$root}/assets': ['{$trunk}/assets']
-        },
-        'commit': [
-          '{$trunk}/js',
-          '{$trunk}/css',
-          '{$trunk}/html',
-          '{$trunk}/images',
-          '{$trunk}/assets'
-        ]
-      }
-
-    }
   }
-  // - 此部分 不要用相对路径
-};
+});
+
+
+// + commit
+Object.assign(config, {
+  commit: {
+    hostname: '//yyweb.yystatic.com/',
+    revAddr: `//yyweb.yystatic.com/${setting.dest.basePath}/${setting.dest.revPath}/rev-manifest.json`
+  }
+});
+// - commit
 
 module.exports = config;
